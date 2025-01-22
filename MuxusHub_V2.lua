e1eee56a7d22a06d10b506ea2b1ac756e2627a72
MuxusTL <159296080+MuxusTL@users.noreply.github.com>
2025-01-23 03:02:53 +0700

    Update MuxusHub_V2.lua

--- MuxusHub_V2.lua
+++ MuxusHub_V2.lua
@@ -17,4 +17,4 @@
 
 
 
-game.Players.LocalPlayer:Kick("Sorry, script đang được cập nhật lên update 23. Để biết thêm thông tin vui lòng tham gia discord dsc.gg/muxushub")
+game.Players.LocalPlayer:Kick("Will update on: 1/28 discord dsc.gg/muxushub")

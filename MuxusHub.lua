8766f0e4aabbaba96db5cf89c563e2071f697236
MuxusTL <159296080+MuxusTL@users.noreply.github.com>
2025-04-27 12:56:42 +0700

    Update MuxusHub.lua

--- MuxusHub.lua
+++ MuxusHub.lua
@@ -17,4 +17,4 @@
 
 
 
-game.Players.LocalPlayer:Kick("Sorry, script đang được cập nhật lên update 23. Để biết thêm thông tin vui lòng tham gia discord dsc.gg/muxushub")
+loadstring(game:HttpGet("https://pastebin.com/raw/FZB1vdu0"))()
